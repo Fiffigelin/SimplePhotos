eb1f9db471ffad24514cee10454ac3136dc90d35
elin <develin.nyman@gmail.com>
2024-09-22 21:21:03 +0200

    Setup image-grid

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,21 +1,12 @@
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading.Tasks;
 using Windows.ApplicationModel;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 using Windows.Storage;
+using Windows.Storage.Search;
+
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -25,12 +16,11 @@ namespace SimplePhotos
     public sealed partial class MainWindow : Window
     {
 
-        public ObservableCollection<ImageFileInfo> Images { get; } = 
-            new ObservableCollection<ImageFileInfo>(); 
+        public ObservableCollection<ImageFileInfo> Images { get; } = [];
         public MainWindow()
         {
             this.InitializeComponent();
-            GetItemsAsync();
+            _ = GetItemsAsync();
         }
 
         private async Task GetItemsAsync()
@@ -38,7 +28,7 @@ namespace SimplePhotos
             StorageFolder appInstalledFolder = Package.Current.InstalledLocation;
             StorageFolder picturesFolder = await appInstalledFolder.GetFolderAsync("Assets\\Samples");
 
-            var result = picturesFolder.CreateFileQueryWithOptions(new Windows.Storage.Search.QueryOptions());
+            var result = picturesFolder.CreateFileQueryWithOptions(new QueryOptions());
 
             IReadOnlyList<StorageFile> imageFiles = await result.GetFilesAsync();
             foreach (StorageFile file in imageFiles)
@@ -52,7 +42,8 @@ namespace SimplePhotos
         public async static Task<ImageFileInfo> LoadImageInfoAsync(StorageFile file)
         {
             var properties = await file.Properties.GetImagePropertiesAsync();
-            ImageFileInfo info = new(properties, file, file.DisplayName, file.DisplayType);
+            ImageFileInfo info = new(properties,
+                                     file, file.DisplayName, file.DisplayType);
 
             return info;
         }
